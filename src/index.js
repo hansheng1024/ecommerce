d524628fb88dc05319050bc1577e03f5aa1e2553
hansheng1024 <hsheng18@usc.edu>
2023-03-10 13:54:57 -0800

    ready for first deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import App from './App';
+import { UserProvider } from './context/user.context'
+import { CategoriesProvider } from './context/categories.context';
+import { CartContext, CartProvider } from './context/cart.context';
+
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  //rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
